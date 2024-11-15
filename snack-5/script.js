8329455e611b3fb010cbec5681a5d56e27afea19
AlessandroSottosanti <asottosanti2@gmail.com>
2024-11-15 16:16:49 +0100

    refactoring: 5th snack

--- snack-5/script.js
+++ snack-5/script.js
@@ -3,13 +3,16 @@ const nums = [2, 8, 4, 7, 12, 87];
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
 
-oddsNumbers = [];
 
-const oddNums = nums.map((num) => {
-    const oddsEven = num % 2 === 0 ? 'Pari' : 'Dispari';
-    if (oddsEven === 'Pari'){
-        oddsNumbers.push(num);
-    }
-  });
+// const oddNums = nums.filter((num) => {
+//     const oddsEven = num % 2 === 0;
+//     if (oddsEven){
+//         return num;
+//     }
+//   });
   
-  console.log (oddsNumbers);
+//   console.log (oddNums);
+
+const evenNums = nums.filter((num) => num % 2 === 0);
+
+console.log (evenNums);
