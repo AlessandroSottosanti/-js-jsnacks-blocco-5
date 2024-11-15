bc0add5d1bbc22058fca7a3e31a5a3c90aec01c8
AlessandroSottosanti <asottosanti2@gmail.com>
2024-11-15 15:59:36 +0100

    feat: 3rd snack

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,9 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const majNumbers = numbers.map((number) => {
+    result = number + 1
+    return result;
+});
+console.log(majNumbers);
