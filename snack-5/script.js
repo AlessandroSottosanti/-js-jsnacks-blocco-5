8cbf7f39dbaf04f9202835c4c3982bff041cdedb
AlessandroSottosanti <asottosanti2@gmail.com>
2024-11-15 16:09:06 +0100

    feat: 5th snack

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,14 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+oddsNumbers = [];
+
+const oddNums = nums.map((num) => {
+    const oddsEven = num % 2 === 0 ? 'Pari' : 'Dispari';
+    if (oddsEven === 'Pari'){
+        oddsNumbers.push(num);
+    }
+  });
+  
+  console.log (oddsNumbers);
